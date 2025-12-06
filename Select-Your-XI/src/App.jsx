59a342932aca7d5bc32a0e27d59b00ced23337eb
Hla Thue Prue Marma <hlathuepruemarma@Hlas-MacBook-Air.local>
2025-12-06 07:22:58 +0300

    Add to navbar

--- Select-Your-XI/src/App.jsx
+++ Select-Your-XI/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import navImg from "./assets/logo.png"
+import coinImg from "./assets/coin.png"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <>
+ <div className="navbar bg-base-100 shadow-sm">
+  
+  <div className="flex-1 ">
+    <img className="w-[60px] h-[60px]" src={navImg} alt="" />
+  </div>
+  <div className="flex items-center">
+     <span>6000000</span>
+     <img className="w-[30px] h-[30px]" src={coinImg} alt="" />
+     
+  </div>
+</div>
+  </>;
 }
 
-export default App
+export default App;
